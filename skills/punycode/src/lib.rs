2f8d2374014ac30c5042f757eac650149d490f73
picoHz <53327867+picoHz@users.noreply.github.com>
2022-03-15 11:38:50 +0900

    skill-punycode: add unit tests

--- skills/punycode/src/lib.rs
+++ skills/punycode/src/lib.rs
@@ -21,20 +21,7 @@ pub struct PunycodeTranslator;
 
 impl Translator for PunycodeTranslator {
     fn parse(&self, args: &[Token]) -> Option<CommandArgs> {
-        let decode = !args.is_empty()
-            && args.iter().all(|token| {
-                token.as_utf8().is_ascii()
-                    && token
-                        .as_utf8()
-                        .to_ascii_lowercase()
-                        .split('.')
-                        .filter(|part| {
-                            part.starts_with("xn--")
-                                && punycode::decode(part.trim_start_matches("xn--")).is_ok()
-                        })
-                        .count()
-                        > 0
-            });
+        let decode = !args.is_empty() && args.iter().all(|token| is_punycode(token.as_utf8()));
 
         if decode {
             return Some(
@@ -57,23 +44,8 @@ impl Command for PunycodeCommand {
 
         let output = args
             .inputs
-            .into_iter()
-            .map(|s| {
-                let s = s
-                    .to_ascii_lowercase()
-                    .split('.')
-                    .map(|part| {
-                        if part.starts_with("xn--") {
-                            punycode::decode(part.trim_start_matches("xn--"))
-                                .ok()
-                                .unwrap_or_default()
-                        } else {
-                            part.to_string()
-                        }
-                    })
-                    .collect::<Vec<_>>();
-                s.join(".")
-            })
+            .iter()
+            .map(|s| encode_to_punycode(s))
             .collect::<Vec<_>>();
 
         Ok(Output {
@@ -100,4 +72,56 @@ struct Args {
 #[derive(ArgEnum, Clone)]
 enum Mode {
     Decode,
+}
+
+fn is_punycode(s: &str) -> bool {
+    s.is_ascii()
+        && s.to_ascii_lowercase()
+            .split('.')
+            .filter(|part| {
+                part.starts_with("xn--")
+                    && punycode::decode(part.trim_start_matches("xn--")).is_ok()
+            })
+            .count()
+            > 0
+}
+
+fn encode_to_punycode(s: &str) -> String {
+    let s = s
+        .to_ascii_lowercase()
+        .split('.')
+        .map(|part| {
+            if part.starts_with("xn--") {
+                punycode::decode(part.trim_start_matches("xn--"))
+                    .ok()
+                    .unwrap_or_default()
+            } else {
+                part.to_string()
+            }
+        })
+        .collect::<Vec<_>>();
+    s.join(".")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_punycode() {
+        assert!(is_punycode("xn--cookie!-1d84f"));
+        assert!(is_punycode("xn--mushroom-bd25gia"));
+        assert!(is_punycode("xn--li8h.and.xn--ri8h"));
+        assert!(is_punycode("XN--COOKIE!-1D84F"));
+        assert!(!is_punycode("example.com"));
+    }
+
+    #[test]
+    fn test_encode_to_punycode() {
+        assert_eq!(encode_to_punycode("xn--cookie!-1d84f"), "cookie🍪!");
+        assert_eq!(encode_to_punycode("XN--COOKIE!-1D84F"), "cookie🍪!");
+        assert_eq!(encode_to_punycode("xn--mushroom-bd25gia"), "🍄mushroom🍄");
+        assert_eq!(encode_to_punycode("xn--li8h.and.xn--ri8h"), "🍋.and.🍑");
+        assert_eq!(encode_to_punycode("example.com"), "example.com");
+    }
 }
