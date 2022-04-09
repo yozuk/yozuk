6ca37c350d82749414e51c308e12be8643aa0f8e
picoHz <53327867+picoHz@users.noreply.github.com>
2022-04-09 22:17:28 +0900

    yozuk-sdk: serialize UTF-8 bytes as a string

--- yozuk-sdk/src/serde_bytes.rs
+++ yozuk-sdk/src/serde_bytes.rs
@@ -1,4 +1,3 @@
-use ascii::AsciiStr;
 use bytes::Bytes;
 use serde_derive::{Deserialize, Serialize};
 use std::str;
@@ -7,8 +6,8 @@ pub fn serialize_bytes<S>(data: &Bytes, serializer: S) -> Result<S::Ok, S::Error
 where
     S: serde::Serializer,
 {
-    if let Ok(str) = AsciiStr::from_ascii(data) {
-        serde::Serialize::serialize(&Data::String(str.as_str()), serializer)
+    if let Ok(s) = str::from_utf8(data) {
+        serde::Serialize::serialize(&Data::String(s), serializer)
     } else {
         serde::Serialize::serialize(
             &Data::Base64(Base64Data {
@@ -26,8 +25,8 @@ where
     use serde::ser::SerializeSeq;
     let mut seq = serializer.serialize_seq(Some(vec.len()))?;
     for data in vec {
-        if let Ok(str) = AsciiStr::from_ascii(data) {
-            seq.serialize_element(&Data::String(str.as_str()))?;
+        if let Ok(s) = str::from_utf8(data) {
+            seq.serialize_element(&Data::String(s))?;
         } else {
             seq.serialize_element(&Data::Base64(Base64Data {
                 base64: &base64::encode(data),
