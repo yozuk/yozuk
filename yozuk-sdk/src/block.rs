3ef2869e55ee7f3279f295e3e2b3214e64d7691b
picoHz <53327867+picoHz@users.noreply.github.com>
2022-04-23 09:53:28 +0900

    wip: data

--- yozuk-sdk/src/block.rs
+++ yozuk-sdk/src/block.rs
@@ -1,3 +1,5 @@
+use crate::serde_bytes::{deserialize_bytes, serialize_bytes};
+use bytes::Bytes;
 use mediatype::{media_type, MediaTypeBuf};
 use serde_derive::{Deserialize, Serialize};
 
@@ -9,9 +11,10 @@ pub struct Block {
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-#[serde(tag = "type")]
+#[serde(tag = "type", rename_all = "snake_case")]
 pub enum BlockElement {
     Comment(Comment),
+    Data(Data),
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
@@ -47,6 +50,48 @@ impl Default for Comment {
         Self {
             text: String::new(),
             media_type: media_type!(TEXT / PLAIN).into(),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct Data {
+    #[serde(
+        serialize_with = "serialize_bytes",
+        deserialize_with = "deserialize_bytes"
+    )]
+    pub data: Bytes,
+
+    pub media_type: MediaTypeBuf,
+}
+
+impl Data {
+    pub fn new() -> Self {
+        Default::default()
+    }
+
+    pub fn set_data<T>(mut self, data: T) -> Self
+    where
+        T: Into<Bytes>,
+    {
+        self.data = data.into();
+        self
+    }
+
+    pub fn set_media_type<T>(mut self, media_type: T) -> Self
+    where
+        T: Into<MediaTypeBuf>,
+    {
+        self.media_type = media_type.into();
+        self
+    }
+}
+
+impl Default for Data {
+    fn default() -> Self {
+        Self {
+            data: Bytes::new(),
+            media_type: media_type!(APPLICATION / OCTET_STREAM).into(),
         }
     }
 }
