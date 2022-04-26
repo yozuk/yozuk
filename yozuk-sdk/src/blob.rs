80f33f1259db0ae3033bafc9e0e63855d41ec49b
picoHz <53327867+picoHz@users.noreply.github.com>
2022-04-26 11:59:00 +0900

    test

--- yozuk-sdk/src/blob.rs
+++ yozuk-sdk/src/blob.rs
@@ -1,7 +1,8 @@
-use blake2::{digest::consts::U10, Blake2b, Digest};
+use blake2::{digest::consts::U12, Blake2b, Digest};
 use bytes::Bytes;
+use std::str;
 
-type Blake2b80 = Blake2b<U10>;
+type Blake2b96 = Blake2b<U12>;
 
 #[derive(Debug, Clone)]
 pub struct Blob {
@@ -21,6 +22,14 @@ impl Blob {
     pub fn data(&self) -> &Bytes {
         &self.data
     }
+
+    pub fn as_utf8(&self) -> &str {
+        if let Ok(str) = str::from_utf8(&self.data) {
+            str
+        } else {
+            ""
+        }
+    }
 }
 
 impl Default for Blob {
@@ -29,11 +38,33 @@ impl Default for Blob {
     }
 }
 
+impl PartialEq for Blob {
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id
+    }
+}
+
 impl From<Bytes> for Blob {
     fn from(data: Bytes) -> Self {
-        let mut hasher = Blake2b80::new();
+        let mut hasher = Blake2b96::new();
         hasher.update(&data);
         let id = base64::encode_config(&hasher.finalize(), base64::URL_SAFE);
         Self { id, data }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_blob() {
+        let blob = Blob::new();
+        assert_eq!(blob.id(), "uOHdo6wKo4IK0pkL");
+
+        let blob = Blob::from(Bytes::from_static(b"Hello World!"));
+        assert_eq!(blob.id(), "VVg4lZh3I00ZfiRa");
+        assert_eq!(blob.data().as_ref(), b"Hello World!");
+        assert_eq!(blob.as_utf8(), "Hello World!");
     }
 }
