c8bff23feffe208a107c626aca5a4a818df6da81
picoHz <53327867+picoHz@users.noreply.github.com>
2022-05-06 23:16:22 +0900

    add methods

--- zuk/src/rpc.rs
+++ zuk/src/rpc.rs
@@ -2,12 +2,13 @@
 
 use anyhow::Result;
 use json_rpc2::{Request, Response, Server, Service};
-use serde_json::{de::IoRead, Deserializer, Value};
+use serde_json::{de::IoRead, Deserializer};
 use std::io::{Read, Write};
 use yozuk::Yozuk;
 use yozuk_sdk::prelude::*;
 
 struct ServiceHandler;
+
 impl Service for ServiceHandler {
     type Data = Yozuk;
     fn handle(&self, request: &Request, zuk: &Self::Data) -> json_rpc2::Result<Option<Response>> {
@@ -16,6 +17,11 @@ impl Service for ServiceHandler {
                 let tokens: Vec<Token> = request.deserialize()?;
                 let commands = zuk.get_commands(&tokens, &[]);
                 Some((request, serde_json::to_value(commands).unwrap()).into())
+            }
+            "run_commands" => {
+                let commands: Vec<CommandArgs> = request.deserialize()?;
+                let result = zuk.run_commands(commands, &mut [], None);
+                Some((request, serde_json::to_value(result).unwrap()).into())
             }
             _ => None,
         };
@@ -28,7 +34,7 @@ where
     R: Read,
     W: Write,
 {
-    let service: Box<dyn Service<Data = Yozuk>> = Box::new(ServiceHandler {});
+    let service: Box<dyn Service<Data = Yozuk>> = Box::new(ServiceHandler);
     let server = Server::new(vec![&service]);
     let reader = IoRead::new(reader);
     let stream = Deserializer::new(reader).into_iter::<Request>();
