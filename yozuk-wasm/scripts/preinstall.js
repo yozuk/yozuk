26685f7b559642ff7299d7dd21258f24506a24b4
picoHz <53327867+picoHz@users.noreply.github.com>
2022-05-29 21:12:03 +0900

    publish yozuk-wasm

--- yozuk-wasm/scripts/preinstall.js
+++ yozuk-wasm/scripts/preinstall.js
@@ -1,9 +1,12 @@
+#!/usr/bin/node
+
 const { execSync } = require('node:child_process');
 
 const options = {
     env: {
         "RUSTFLAGS": "-C opt-level=z"
-    }
-}
+    },
+    stdio: 'inherit'
+};
 
-execSync('wasm-pack build -d wasm-web -t web --release', { stdio: 'inherit' });
+execSync('wasm-pack build -d wasm-web -t web --release', options);
