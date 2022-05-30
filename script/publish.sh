7826a79a829b62419601f0b12897b0dc6b8c072d
picoHz <53327867+picoHz@users.noreply.github.com>
2022-05-30 23:41:25 +0900

    publish.sh: remove --dry-run

--- script/publish.sh
+++ script/publish.sh
@@ -35,7 +35,7 @@ publishCrate "yozuk" "yozuk"
 publishCrate "zuk" "zuk"
 
 sed -i -E "0,/version/ s/\"version\": \"[.0-9]+\"/\"version\": \"${NEXT_TAG#v}\"/" yozuk-wasm/package.json
-npm publish ./yozuk-wasm --dry-run
+npm publish ./yozuk-wasm
 git commit -a -m "publish yozuk-wasm $NEXT_TAG"
 
 git tag $NEXT_TAG
