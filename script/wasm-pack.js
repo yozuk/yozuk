e78788624d3043bc9c8d85544a4ffe3cff98e17b
picoHz <53327867+picoHz@users.noreply.github.com>
2022-05-30 10:40:51 +0900

    add wasm-pack.js

---
+++ script/wasm-pack.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+const { execSync } = require('node:child_process');
+
+const options = {
+    cwd: `${__dirname}/../yozuk-wasm`,
+    env: {
+        ...process.env,
+        "RUSTFLAGS": "-C opt-level=z"
+    },
+    stdio: 'inherit'
+};
+
+function build(out, target, profile, env = {}) {
+    const dir = `${__dirname}/../yozuk-wasm`;
+    const options = {
+        cwd: dir,
+        env: {
+            ...process.env,
+            ...env,
+            "RUSTFLAGS": "-C opt-level=z"
+        },
+        stdio: 'inherit'
+    };
+    execSync(`wasm-pack build -d ${out} -t ${target} --${profile}`, options);
+    execSync(`tar -C ${dir} -Jcvf yozuk-${out}.tar.xz ${out}`);
+}
+
+build('wasm-web', 'web', 'release', { "RUSTFLAGS": "-C opt-level=z" });
+build('wasm-web-debug', 'web', 'debug');
