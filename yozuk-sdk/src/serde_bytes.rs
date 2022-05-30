5756d727186afb1612b652d57b901c6eaada83b0
picoHz <53327867+picoHz@users.noreply.github.com>
2022-05-30 20:19:26 +0900

    yozuk-sdk: always use base64 for serializing Bytes

--- yozuk-sdk/src/serde_bytes.rs
+++ yozuk-sdk/src/serde_bytes.rs
@@ -1,21 +1,12 @@
 use bytes::Bytes;
-use serde_derive::{Deserialize, Serialize};
+use std::borrow::Cow;
 use std::str;
 
 pub fn serialize_bytes<S>(data: &Bytes, serializer: S) -> Result<S::Ok, S::Error>
 where
     S: serde::Serializer,
 {
-    if let Ok(s) = str::from_utf8(data) {
-        serde::Serialize::serialize(&Data::String(s), serializer)
-    } else {
-        serde::Serialize::serialize(
-            &Data::Base64(Base64Data {
-                base64: &base64::encode(data),
-            }),
-            serializer,
-        )
-    }
+    serde::Serialize::serialize(&base64::encode(data), serializer)
 }
 
 pub fn serialize_bytes_vec<S>(vec: &[Bytes], serializer: S) -> Result<S::Ok, S::Error>
@@ -25,13 +16,7 @@ where
     use serde::ser::SerializeSeq;
     let mut seq = serializer.serialize_seq(Some(vec.len()))?;
     for data in vec {
-        if let Ok(s) = str::from_utf8(data) {
-            seq.serialize_element(&Data::String(s))?;
-        } else {
-            seq.serialize_element(&Data::Base64(Base64Data {
-                base64: &base64::encode(data),
-            }))?;
-        }
+        seq.serialize_element(&base64::encode(data))?;
     }
     seq.end()
 }
@@ -41,12 +26,10 @@ where
     D: serde::Deserializer<'de>,
 {
     use serde::de::Error;
-    let data: Data = serde::Deserialize::deserialize(deserializer)?;
-    Ok(match data {
-        Data::String(str) => Bytes::from(str.to_string()),
-        Data::Base64(data) => Bytes::from(base64::decode(data.base64).map_err(Error::custom)?),
-        Data::Bytes(data) => data,
-    })
+    let data: Cow<'de, str> = serde::Deserialize::deserialize(deserializer)?;
+    Ok(Bytes::from(
+        base64::decode(data.as_ref()).map_err(Error::custom)?,
+    ))
 }
 
 pub fn deserialize_bytes_vec<'de, D>(deserializer: D) -> Result<Vec<Bytes>, D::Error>
@@ -54,27 +37,12 @@ where
     D: serde::Deserializer<'de>,
 {
     use serde::de::Error;
-    let vec: Vec<Data> = serde::Deserialize::deserialize(deserializer)?;
+    let vec: Cow<'de, [Cow<'de, str>]> = serde::Deserialize::deserialize(deserializer)?;
     let mut bytes: Vec<Bytes> = Vec::with_capacity(vec.len());
-    for data in vec {
-        bytes.push(match data {
-            Data::String(str) => Bytes::from(str.to_string()),
-            Data::Base64(data) => Bytes::from(base64::decode(data.base64).map_err(Error::custom)?),
-            Data::Bytes(data) => data,
-        });
+    for data in vec.as_ref() {
+        bytes.push(Bytes::from(
+            base64::decode(data.as_ref()).map_err(Error::custom)?,
+        ));
     }
     Ok(bytes)
-}
-
-#[derive(Serialize, Deserialize)]
-#[serde(untagged)]
-enum Data<'a> {
-    String(&'a str),
-    Base64(Base64Data<'a>),
-    Bytes(Bytes),
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Base64Data<'a> {
-    base64: &'a str,
 }
