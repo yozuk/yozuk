b46febf3c0178d40cabe58cbc4c7a441accc60c3
picoHz <53327867+picoHz@users.noreply.github.com>
2022-07-28 12:31:00 +0900

    yozuk-wasm: fix member visibility in Yozuk

--- yozuk-wasm/web.ts
+++ yozuk-wasm/web.ts
@@ -11,27 +11,27 @@ async function init_once() {
 }
 
 export class Yozuk extends YozukBase {
-    exec_impl(command: string, i18n: string): Promise<string> {
+    protected exec_impl(command: string, i18n: string): Promise<string> {
         return init_once().then(() => exec(command, i18n));
     }
 
-    push_stream_impl(stream: Uint8Array): Promise<void> {
+    protected push_stream_impl(stream: Uint8Array): Promise<void> {
         return init_once().then(() => push_stream(stream));
     }
 
-    random_suggestions_impl(amount: number): Promise<string> {
+    protected random_suggestions_impl(amount: number): Promise<string> {
         return init_once().then(() => random_suggestions(amount));
     }
 
-    push_suggestions_stream_impl(stream: Uint8Array): Promise<void> {
+    protected push_suggestions_stream_impl(stream: Uint8Array): Promise<void> {
         return init_once().then(() => push_suggestions_stream(stream));
     }
 
-    clear_suggestions_stream_impl(): Promise<void> {
+    protected clear_suggestions_stream_impl(): Promise<void> {
         return init_once().then(() => clear_suggestions_stream());
     }
 
-    suggestions_impl(command: string, amount: number): Promise<string> {
+    protected suggestions_impl(command: string, amount: number): Promise<string> {
         return init_once().then(() => suggestions(command, amount));
     }
 
