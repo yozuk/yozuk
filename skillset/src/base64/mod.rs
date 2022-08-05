e952cf1cbcc03b6c27a3614f7bfb1eaa9d0b6f62
picoHz <53327867+picoHz@users.noreply.github.com>
2022-08-05 20:13:14 +0900

    skill-base64: improve suggestions

--- skillset/src/base64/mod.rs
+++ skillset/src/base64/mod.rs
@@ -32,12 +32,15 @@ impl Suggestions for Base64Suggestions {
             .filter(|arg| arg.tag == "input:data")
             .map(|arg| arg.as_str())
             .collect::<Vec<_>>();
-        if !inputs.is_empty() {
-            vec![format!("{} to Base64", shell_words::join(inputs))]
-        } else if !streams.is_empty() {
+        if !streams.is_empty() {
             vec!["Base64".to_string()]
         } else {
-            vec![]
+            let joined = shell_words::join(if inputs.is_empty() {
+                vec!["Hello World!"]
+            } else {
+                inputs
+            });
+            vec![format!("{joined} to Base64")]
         }
     }
 }
