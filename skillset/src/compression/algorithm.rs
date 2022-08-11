dbe3ffd53d3b4e519f01f375d02651d6dc577f38
picoHz <53327867+picoHz@users.noreply.github.com>
2022-08-11 18:28:14 +0900

    add skill-compression

---
+++ skillset/src/compression/algorithm.rs
@@ -0,0 +1,101 @@
+use flate2::write::{DeflateEncoder, GzEncoder, ZlibEncoder};
+use flate2::Compression;
+use std::io::Write;
+
+pub struct Algorithm {
+    pub name: &'static str,
+    pub keywords: &'static [&'static str],
+    pub compressor: fn() -> Box<dyn Compressor>,
+}
+
+pub const ENTRIES: &[Algorithm] = &[
+    Algorithm {
+        name: "Zlib",
+        keywords: &["zlib"],
+        compressor: || Box::new(ZlibCompressor::new()),
+    },
+    Algorithm {
+        name: "Gzip",
+        keywords: &["gzip", "gz"],
+        compressor: || Box::new(GzipCompressor::new()),
+    },
+    Algorithm {
+        name: "Deflate",
+        keywords: &["deflate"],
+        compressor: || Box::new(DeflateCompressor::new()),
+    },
+];
+
+pub trait Compressor {
+    fn update(&mut self, data: &[u8]);
+    fn finalize(&mut self) -> Vec<u8>;
+}
+
+struct ZlibCompressor(Option<ZlibEncoder<Vec<u8>>>);
+
+impl ZlibCompressor {
+    fn new() -> Self {
+        Self(Some(ZlibEncoder::new(Vec::new(), Compression::fast())))
+    }
+}
+
+impl Compressor for ZlibCompressor {
+    fn update(&mut self, data: &[u8]) {
+        if let Some(inner) = &mut self.0 {
+            inner.write_all(data).unwrap();
+        }
+    }
+
+    fn finalize(&mut self) -> Vec<u8> {
+        self.0
+            .take()
+            .and_then(|inner| inner.finish().ok())
+            .unwrap_or_default()
+    }
+}
+
+struct GzipCompressor(Option<GzEncoder<Vec<u8>>>);
+
+impl GzipCompressor {
+    fn new() -> Self {
+        Self(Some(GzEncoder::new(Vec::new(), Compression::fast())))
+    }
+}
+
+impl Compressor for GzipCompressor {
+    fn update(&mut self, data: &[u8]) {
+        if let Some(inner) = &mut self.0 {
+            inner.write_all(data).unwrap();
+        }
+    }
+
+    fn finalize(&mut self) -> Vec<u8> {
+        self.0
+            .take()
+            .and_then(|inner| inner.finish().ok())
+            .unwrap_or_default()
+    }
+}
+
+struct DeflateCompressor(Option<DeflateEncoder<Vec<u8>>>);
+
+impl DeflateCompressor {
+    fn new() -> Self {
+        Self(Some(DeflateEncoder::new(Vec::new(), Compression::fast())))
+    }
+}
+
+impl Compressor for DeflateCompressor {
+    fn update(&mut self, data: &[u8]) {
+        if let Some(inner) = &mut self.0 {
+            inner.write_all(data).unwrap();
+        }
+    }
+
+    fn finalize(&mut self) -> Vec<u8> {
+        self.0
+            .take()
+            .and_then(|inner| inner.finish().ok())
+            .unwrap_or_default()
+    }
+}
---
+++ skillset/src/compression/mod.rs
@@ -0,0 +1,171 @@
+use clap::Parser;
+use itertools::iproduct;
+use std::io::Read;
+use yozuk_helper_encoding::EncodingPreprocessor;
+use yozuk_helper_english::normalized_eq;
+use yozuk_sdk::encoding::RawEncoding;
+use yozuk_sdk::prelude::*;
+
+mod algorithm;
+use algorithm::ENTRIES;
+
+pub const ENTRY: SkillEntry = SkillEntry {
+    model_id: b"~VZmWCW5xz8V5Y_NdN6n2",
+    init: |_| {
+        Skill::builder()
+            .add_corpus(CompressionCorpus)
+            .add_preprocessor(EncodingPreprocessor::new(RawEncoding::all()))
+            .add_suggestions(CompressionSuggestions)
+            .add_translator(CompressionTranslator)
+            .set_command(CompressionCommand)
+            .build()
+    },
+};
+
+pub struct CompressionSuggestions;
+
+impl Suggestions for CompressionSuggestions {
+    fn suggestions(&self, _seed: u64, args: &[Token], _streams: &[InputStream]) -> Vec<String> {
+        let inputs = args
+            .iter()
+            .filter(|arg| arg.tag == "input:data")
+            .map(|arg| arg.as_str())
+            .collect::<Vec<_>>();
+        let joined = shell_words::join(if inputs.is_empty() {
+            vec!["Hello World!"]
+        } else {
+            inputs
+        });
+        ENTRIES
+            .iter()
+            .filter_map(|entry| entry.keywords.iter().next())
+            .map(|s| format!("{joined} to {s}"))
+            .collect()
+    }
+}
+
+pub struct CompressionCorpus;
+
+impl Corpus for CompressionCorpus {
+    fn training_data(&self) -> Vec<Vec<Token>> {
+        let inputs = vec![
+            "Hello World",
+            "😍😗😋",
+            "quick brown fox jumps over the lazy dog",
+            "Veterinarian",
+        ];
+        iproduct!(
+            inputs.clone(),
+            ["as", "to", "in", "into"],
+            ENTRIES.iter().flat_map(|entry| entry.keywords)
+        )
+        .map(|(data, prefix, alg)| {
+            tk!([
+                data; "input:data",
+                prefix,
+                *alg; "input:alg"
+            ])
+        })
+        .chain(
+            ENTRIES
+                .iter()
+                .flat_map(|entry| entry.keywords)
+                .map(|alg| tk!([*alg; "input:alg"])),
+        )
+        .collect()
+    }
+}
+
+pub struct CompressionTranslator;
+
+impl Translator for CompressionTranslator {
+    fn generate_command(&self, args: &[Token], _streams: &[InputStream]) -> Option<CommandArgs> {
+        let input = args
+            .iter()
+            .filter(|arg| arg.tag == "input:data")
+            .flat_map(|arg| ["--input", arg.as_str()]);
+
+        let keywords = args
+            .iter()
+            .filter(|arg| arg.tag == "input:alg")
+            .collect::<Vec<_>>();
+
+        if !keywords.is_empty()
+            && keywords.iter().all(|arg| {
+                ENTRIES
+                    .iter()
+                    .any(|entry| normalized_eq(arg.as_str(), entry.keywords, 0))
+            })
+        {
+            return Some(
+                CommandArgs::new().add_args_iter(input).add_args_iter(
+                    keywords
+                        .iter()
+                        .flat_map(|arg| ["--algorithm", arg.as_str()]),
+                ),
+            );
+        }
+
+        None
+    }
+}
+
+pub struct CompressionCommand;
+
+impl Command for CompressionCommand {
+    fn run(
+        &self,
+        args: CommandArgs,
+        streams: &mut [InputStream],
+        _user: &UserContext,
+    ) -> Result<Output, CommandError> {
+        let args = Args::try_parse_from(args.args)?;
+
+        let matched = ENTRIES
+            .iter()
+            .find(|entry| normalized_eq(&args.algorithm, entry.keywords, 0));
+
+        let docs = Metadata::docs("https://docs.yozuk.com/docs/skills/compression/")?;
+
+        if let Some(alg) = matched {
+            let mut compressor = (alg.compressor)();
+            if let [input, ..] = &args.input[..] {
+                compressor.update(input.as_bytes());
+            } else if let [stream, ..] = streams {
+                let mut data = vec![0; 1024];
+                while let Ok(len) = stream.read(&mut data) {
+                    if len == 0 {
+                        break;
+                    }
+                    compressor.update(&data[..len]);
+                }
+            }
+            return Ok(Output::new()
+                .set_title("Compression")
+                .add_block(block::Data::new().set_data(compressor.finalize()))
+                .add_metadata(docs));
+        }
+
+        Err(Output::new()
+            .set_title("Compression")
+            .add_metadata(docs)
+            .add_block(
+                block::Comment::new().set_text(format!("Unsupprted algorithm: {}", args.algorithm)),
+            )
+            .into())
+    }
+
+    fn priority(&self) -> i32 {
+        -120
+    }
+}
+
+#[derive(Parser)]
+#[clap(trailing_var_arg = true)]
+pub struct Args {
+    #[clap(long)]
+    pub algorithm: String,
+
+    #[clap(short, long, multiple_occurrences(true))]
+    pub input: Vec<String>,
+}
