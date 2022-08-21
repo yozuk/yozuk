02ec2b103910166639a4223e65b509bc6ae898cc
picoHz <picoHz@outlook.com>
2022-08-21 22:15:18 +0900

    skill-compression: support decompression

--- skillset/src/compression/algorithm.rs
+++ skillset/src/compression/algorithm.rs
@@ -1,11 +1,15 @@
-use flate2::write::{DeflateEncoder, GzEncoder, ZlibEncoder};
+use flate2::write::{
+    DeflateDecoder, DeflateEncoder, GzDecoder, GzEncoder, ZlibDecoder, ZlibEncoder,
+};
 use flate2::Compression;
-use std::io::Write;
+use std::io::{Read, Result, Write};
 
 pub struct Algorithm {
     pub name: &'static str,
     pub keywords: &'static [&'static str],
     pub compressor: fn() -> Box<dyn Compressor>,
+    pub test_header: fn(&[u8]) -> bool,
+    pub decompressor: fn() -> Box<dyn Decompressor>,
 }
 
 pub const ENTRIES: &[Algorithm] = &[
@@ -13,27 +17,40 @@ pub const ENTRIES: &[Algorithm] = &[
         name: "Zlib",
         keywords: &["zlib"],
         compressor: || Box::new(ZlibCompressor::new()),
+        test_header: |_| false,
+        decompressor: || Box::new(ZlibDecompressor::new()),
     },
     Algorithm {
         name: "Gzip",
         keywords: &["gzip", "gz"],
         compressor: || Box::new(GzipCompressor::new()),
+        test_header: |header| header.starts_with(&[0x1f, 0x8b, 0x08]),
+        decompressor: || Box::new(GzipDecompressor::new()),
     },
     Algorithm {
         name: "Deflate",
         keywords: &["deflate"],
         compressor: || Box::new(DeflateCompressor::new()),
+        test_header: |_| false,
+        decompressor: || Box::new(DeflateDecompressor::new()),
     },
     Algorithm {
         name: "Snappy",
         keywords: &["snappy"],
         compressor: || Box::new(SnappyCompressor::new()),
+        test_header: |_| false,
+        decompressor: || Box::new(SnappyDecompressor::new()),
     },
 ];
 
 pub trait Compressor {
     fn update(&mut self, data: &[u8]);
     fn finalize(&mut self) -> Vec<u8>;
+}
+
+pub trait Decompressor {
+    fn update(&mut self, data: &[u8]) -> Result<()>;
+    fn finalize(&mut self) -> Result<Vec<u8>>;
 }
 
 struct ZlibCompressor(Option<ZlibEncoder<Vec<u8>>>);
@@ -56,6 +73,28 @@ impl Compressor for ZlibCompressor {
             .take()
             .and_then(|inner| inner.finish().ok())
             .unwrap_or_default()
+    }
+}
+
+struct ZlibDecompressor(Option<ZlibDecoder<Vec<u8>>>);
+
+impl ZlibDecompressor {
+    fn new() -> Self {
+        Self(Some(ZlibDecoder::new(Vec::new())))
+    }
+}
+
+impl Decompressor for ZlibDecompressor {
+    fn update(&mut self, data: &[u8]) -> Result<()> {
+        if let Some(inner) = &mut self.0 {
+            inner.write_all(data)?;
+        }
+        Ok(())
+    }
+
+    fn finalize(&mut self) -> Result<Vec<u8>> {
+        let inner = self.0.take().unwrap();
+        inner.finish()
     }
 }
 
@@ -82,6 +121,28 @@ impl Compressor for GzipCompressor {
     }
 }
 
+struct GzipDecompressor(Option<GzDecoder<Vec<u8>>>);
+
+impl GzipDecompressor {
+    fn new() -> Self {
+        Self(Some(GzDecoder::new(Vec::new())))
+    }
+}
+
+impl Decompressor for GzipDecompressor {
+    fn update(&mut self, data: &[u8]) -> Result<()> {
+        if let Some(inner) = &mut self.0 {
+            inner.write_all(data)?;
+        }
+        Ok(())
+    }
+
+    fn finalize(&mut self) -> Result<Vec<u8>> {
+        let inner = self.0.take().unwrap();
+        inner.finish()
+    }
+}
+
 struct DeflateCompressor(Option<DeflateEncoder<Vec<u8>>>);
 
 impl DeflateCompressor {
@@ -105,6 +166,28 @@ impl Compressor for DeflateCompressor {
     }
 }
 
+struct DeflateDecompressor(Option<DeflateDecoder<Vec<u8>>>);
+
+impl DeflateDecompressor {
+    fn new() -> Self {
+        Self(Some(DeflateDecoder::new(Vec::new())))
+    }
+}
+
+impl Decompressor for DeflateDecompressor {
+    fn update(&mut self, data: &[u8]) -> Result<()> {
+        if let Some(inner) = &mut self.0 {
+            inner.write_all(data)?;
+        }
+        Ok(())
+    }
+
+    fn finalize(&mut self) -> Result<Vec<u8>> {
+        let inner = self.0.take().unwrap();
+        inner.finish()
+    }
+}
+
 struct SnappyCompressor(Option<snap::write::FrameEncoder<Vec<u8>>>);
 
 impl SnappyCompressor {
@@ -125,5 +208,28 @@ impl Compressor for SnappyCompressor {
             .take()
             .and_then(|inner| inner.into_inner().ok())
             .unwrap_or_default()
+    }
+}
+
+struct SnappyDecompressor(Vec<u8>);
+
+impl SnappyDecompressor {
+    fn new() -> Self {
+        Self(Vec::new())
+    }
+}
+
+impl Decompressor for SnappyDecompressor {
+    fn update(&mut self, data: &[u8]) -> Result<()> {
+        self.0.extend_from_slice(data);
+        Ok(())
+    }
+
+    fn finalize(&mut self) -> Result<Vec<u8>> {
+        let data: &[u8] = &self.0;
+        let mut buf = Vec::new();
+        let mut decoder = snap::read::FrameDecoder::new(data);
+        let _ = decoder.read_to_end(&mut buf)?;
+        Ok(buf)
     }
 }
